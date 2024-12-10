e1aabc2721cbe2dcc2e6042b929415b53f9abf1d
Homaland <168760948+Homaland@users.noreply.github.com>
2024-12-10 09:38:50 +0500

    Update tonapi.ts

--- src/tonapi.ts
+++ src/tonapi.ts
@@ -2,7 +2,7 @@ import { TonApiClient } from "@ton-api/client";
 
 const ta = new TonApiClient({
   baseUrl: "https://tonapi.io",
-  apiKey: import.meta.env.VITE_TONAPI_KEY || undefined,
+  apiKey: "AENQOALBK32IMSIAAAADVAKASWPDEMFEDMG6AUAEX6A432B4ODXPICZJAUOFJUCHEXFP3HY",
 });
 
 export default ta;
