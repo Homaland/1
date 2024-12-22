40dd6cf5e64190af17b99b5d6dd115d28f7bd0cd
Homaland <168760948+Homaland@users.noreply.github.com>
2024-12-22 20:26:24 +0500

    Delete src/src directory

--- src/src/tonapi.ts
+++
@@ -1,8 +0,0 @@
-import { TonApiClient } from "@ton-api/client";
-
-const ta = new TonApiClient({
-  baseUrl: "https://tonapi.io",
-  apiKey: "AENQOALBK32IMSIAAAADVAKASWPDEMFEDMG6AUAEX6A432B4ODXPICZJAUOFJUCHEXFP3HY",
-});
-
-export default ta;
