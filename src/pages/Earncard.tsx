c91534cda258c830c8c7bb37844ec1670a86a277
Homaland <168760948+Homaland@users.noreply.github.com>
2025-01-02 17:57:35 +0500

    Update Earncard.tsx

--- src/pages/Earncard.tsx
+++ src/pages/Earncard.tsx
@@ -2,6 +2,28 @@
 import React from "react";
 import BottomMenu from "../components/BottomMenu";
 
+const [balance, setBalance] = useState<number | null>(null);
+
+ useEffect(() => { 
+  if (WebApp.initDataUnsafe && WebApp.initDataUnsafe.user) {
+    const telegramId = WebApp.initDataUnsafe.user.id;
+    console.log("Telegram ID:", telegramId);  // Логируем ID пользователя
+
+    fetch(`https://homaland-hodrland-04a0.twc1.net/get_balance/${telegramId}`) // Используем обратные кавычки
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.status === "success") {
+          setBalance(data.balance);
+        } else {
+          console.error("Error fetching balance:", data.message);
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching balance:", error);
+      });
+  }
+}, []);
+
 const EarnPage: React.FC = () => {
   return (
   
