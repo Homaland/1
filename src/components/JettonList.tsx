1af325b69d8342b5dd5d243906affe70cc7889e6
Homaland <168760948+Homaland@users.noreply.github.com>
2025-01-02 21:39:26 +0500

    Update JettonList.tsx

--- src/components/JettonList.tsx
+++ src/components/JettonList.tsx
@@ -1,182 +1,115 @@
-import { useEffect, useState } from "react"; 
-import { JettonBalance } from "@ton-api/client";
-import { JettonItem } from "./JettonItem";
+import React, { useEffect, useState } from "react";
+import ApexCharts from "react-apexcharts";
+import BottomMenu from "../components/BottomMenu";
+import "./PlayPage.css";
 
-interface JettonListProps {
-  jettons: JettonBalance[] | null;
-  connectedAddressString: string | null;
-  onSendClick: (jetton: JettonBalance) => void;
-  className?: string;
-}
-
-const getTonPriceInUSD = async () => {
-  try {
-    const response = await fetch("https://api.coingecko.com/api/v3/simple/price?ids=the-open-network&vs_currencies=usd");
-    const data = await response.json();
-    return data["the-open-network"]?.usd || 0;
-  } catch (error) {
-    console.error("Ошибка при получении курса TON:", error);
-    return 0;
-  }
-};
-
-const getTonBalance = async (address: string) => {
-  try {
-    const response = await fetch(`https://toncenter.com/api/v2/getAddressBalance?address=${address}`);
-    const data = await response.json();
-    if (data.ok) {
-      return data.result / 1e9;
-    } else {
-      throw new Error("Ошибка при получении баланса");
-    }
-  } catch (error) {
-    console.error(error);
-    return null;
-  }
-};
-
-export const JettonList = ({ jettons, connectedAddressString, onSendClick, className }: JettonListProps) => {
-  const [tonBalance, setTonBalance] = useState<number | null>(null);
-  const [tonPriceInUSD, setTonPriceInUSD] = useState<number>(0);
-  const [loading, setLoading] = useState(true);
+const PlayPage: React.FC = () => {
+  const [series, setSeries] = useState([{ data: [] as { x: number; y: number }[] }]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [price, setPrice] = useState<number | null>(null);
 
   useEffect(() => {
     const fetchData = async () => {
-      setLoading(true);
       try {
-        if (connectedAddressString) {
-          const balance = await getTonBalance(connectedAddressString);
-          const price = await getTonPriceInUSD();
-          setTonBalance(balance);
-          setTonPriceInUSD(price);
-        }
+        const response = await fetch("https://api.binance.com/api/v3/ticker/price?symbol=TONUSDT");
+        const data = await response.json();
+        const currentPrice = parseFloat(data.price);
+        setPrice(currentPrice); // Store the current price
+        const time = new Date().getTime();
+
+        setSeries((prev) => {
+          const updatedData = [...prev[0].data, { x: time, y: currentPrice }];
+          if (updatedData.length > 50) updatedData.shift();
+          return [{ data: updatedData }];
+        });
+
+        setIsLoading(false); // Снимаем флаг загрузки
       } catch (error) {
-        console.error(error);
-      } finally {
-        setLoading(false);
+        console.error("Error fetching data:", error);
       }
     };
 
+    const interval = setInterval(fetchData, 3500);
     fetchData();
-  }, [connectedAddressString]);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const options: ApexCharts.ApexOptions = {
+    chart: {
+      type: "line",
+      animations: {
+        enabled: true,
+        dynamicAnimation: {
+          enabled: true,
+          speed: 2000,
+        },
+      },
+      toolbar: { show: false },
+      zoom: { enabled: false },
+      background: "transparent",
+    },
+    stroke: {
+      curve: "smooth",
+      width: 3,
+      colors: ["#FFC107"],
+    },
+    xaxis: {
+      type: "datetime",
+      labels: { show: false },
+      axisTicks: { show: false },
+      axisBorder: { show: false },
+    },
+    yaxis: {
+      labels: {
+        formatter: (value) => `$${value.toFixed(2)}`,
+        style: { colors: "#FFC107" },
+      },
+    },
+    grid: { show: false },
+    tooltip: { enabled: false },
+    colors: ["#FFC107"],
+  };
 
   return (
-    <div className={className}>
-      {connectedAddressString ? (
-        <div>
-          {loading ? (
-            <div className="skeleton-list">
-              <div className="skeleton-item">
-                <div className="skeleton-image" />
-                <div className="skeleton-details">
-                  <div className="skeleton-line" style={{ width: "100px", height: "16px" }} />
-                  <div className="skeleton-line" style={{ width: "50px", height: "14px" }} />
-                </div>
-              </div>
-              <div className="skeleton-item">
-                <div className="skeleton-image" />
-                <div className="skeleton-details">
-                  <div className="skeleton-line" style={{ width: "100px", height: "16px" }} />
-                  <div className="skeleton-line" style={{ width: "50px", height: "14px" }} />
-                </div>
-              </div>
-            </div>
-          ) : tonBalance !== null && tonPriceInUSD > 0 ? (
-            <div className="jetton-item">
+    <div className="play-page">
+      {/* Добавляем картинку перед заголовком */}
+      <img
+        src="https://raw.githubusercontent.com/HODRLAND/HODR/refs/heads/main/img/IMG_0189.png"
+        alt="Earn"
+        style={{ display: "block", margin: "0 auto", maxWidth: "100%", height: "auto" }}
+      />
+      <h1 style={{ textAlign: "center" }}>Earn</h1>
+      <div className="earn-blok-wrapper">
+        {isLoading ? (
+          <div className="skeleton-loader"></div>
+        ) : (
+          <div className="chart-container">
+            <div className="chart-text top" style={{ display: "flex", alignItems: "center" }}>
+              {/* TON Logo and Current Price */}
               <img
                 src="https://ton.org/download/ton_symbol.svg"
                 alt="TON"
                 className="jetton-image"
-                style={{ width: "40px", height: "40px", borderRadius: "50%" }}
+                style={{ width: "20px", height: "20px", borderRadius: "50%" }}
               />
-              <div className="jetton-details">
-                <p>TON: </p>
-              </div>
-              <p>{tonBalance}</p>
-              <p>({(tonBalance * tonPriceInUSD).toFixed(2)} $)</p>
+              TON {price !== null ? `$${price.toFixed(2)}` : "Loading..."}
             </div>
-          ) : (
-            <p>No jettons found</p>
-          )}
-          {jettons && jettons.length ? (
-            jettons
-              .filter((jettonBalance) => jettonBalance.balance > 0)
-              .map((jettonBalance) => (
-                <JettonItem key={jettonBalance.jetton.address.toString()} jettonBalance={jettonBalance} onSendClick={onSendClick} />
-              ))
-          ) : (
-            <p>No jettons found</p>
-          )}
-        </div>
-      ) : (
-        <div className="connect-message">
-          {loading ? (
-            <div className="skeleton-text" />
-          ) : (
-            <p>Connect to see jettons</p>
-          )}
-        </div>
-      )}
-      <style>
-        {`
-          @keyframes skeleton-loading {
-            0% {
-              background-color: #e0e0e0;
-            }
-            50% {
-              background-color: #f7f7f7;
-            }
-            100% {
-              background-color: #e0e0e0;
-            }
-          }
-
-          .skeleton-list {
-            display: flex;
-            flex-direction: column;
-            gap: 16px;
-          }
-          .skeleton-item {
-            display: flex;
-            align-items: center;
-            gap: 10px;
-          }
-          .skeleton-image {
-            width: 40px;
-            height: 40px;
-            background-color: #e0e0e0;
-            border-radius: 50%;
-            animation: skeleton-loading 1.5s infinite ease-in-out;
-          }
-          .skeleton-details {
-            display: flex;
-            flex-direction: column;
-            gap: 5px;
-          }
-          .skeleton-line {
-            background-color: #e0e0e0;
-            border-radius: 4px;
-            animation: skeleton-loading 1.5s infinite ease-in-out;
-          }
-          .connect-message {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100%;
-            text-align: center;
-            font-size: 18px;
-            color: #888;
-          }
-          .skeleton-text {
-            width: 200px;
-            height: 20px;
-            background-color: #e0e0e0;
-            border-radius: 4px;
-            margin: 0 auto;
-            animation: skeleton-loading 1.5s infinite ease-in-out;
-          }
-        `}
-      </style>
+            <div className="earn-blok1 loaded">
+              <div style={{ width: "100%", margin: "auto" }}>
+                <ApexCharts options={options} series={series} type="line" />
+              </div>
+            </div>
+            <p className="chart-text bottom">Bot Trading</p> {/* Нижний текст */}
+          </div>
+        )}
+      </div>
+      <div className="how-it-works2">
+        <p>SOON SOON SOON SOON SOON SOON SOON SOON </p>
+      </div>
+      <BottomMenu />
     </div>
   );
 };
+
+export default PlayPage;
