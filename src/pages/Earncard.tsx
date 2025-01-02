67600b9dbbd10dd7f7077665eeb46f36943e1d24
Homaland <168760948+Homaland@users.noreply.github.com>
2025-01-02 17:56:32 +0500

    Update Earncard.tsx

--- src/pages/Earncard.tsx
+++ src/pages/Earncard.tsx
@@ -1,1 +1,17 @@
+// pages/EarnPage.tsx
+import React from "react";
+import BottomMenu from "../components/BottomMenu";
 
+const EarnPage: React.FC = () => {
+  return (
+  
+      {balance !== null ? (
+        <p>Your points: {balance}</p>
+      ) : (
+        <p>Loading your points...</p>
+      )}
+
+  );
+};
+
+export default EarnPage;
