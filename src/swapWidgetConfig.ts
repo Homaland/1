5951caf4c24e3567fedbf4402d616edcf3a4753e
Homaland <168760948+Homaland@users.noreply.github.com>
2025-01-03 14:29:48 +0500

    Update swapWidgetConfig.ts

--- src/swapWidgetConfig.ts
+++ src/swapWidgetConfig.ts
@@ -2,7 +2,7 @@
 import { THEME } from "@tonconnect/ui";
 
 // TonConnect manifest URL
-const manifestUrl = "https://swap.coffee/tonconnect-manifest.json";
+const manifestUrl = "https://homaland-memefight-f32c.twc1.net/static/tonconnect-manifest.json";
 
 // TonConnect конфигурация
 export const tonConnectConfig = {
