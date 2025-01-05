ad423a6ce9f26375218774b437e50007ca5a68ed
Homaland <168760948+Homaland@users.noreply.github.com>
2025-01-05 07:27:17 +0500

    Add files via upload

---
+++ src/components/custom.d.ts
@@ -0,0 +1,4 @@
+declare module '@swap-coffee/ui-sdk' {
+    export function createSwapWidget(selector: string, options: any): void;
+    // Добавьте сюда другие функции или интерфейсы, если они есть
+}
