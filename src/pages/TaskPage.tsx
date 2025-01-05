71702423de03fc66368e8e608e9b38f7cb7f6c3e
Homaland <168760948+Homaland@users.noreply.github.com>
2025-01-05 07:13:09 +0500

    Add files via upload

--- src/pages/TaskPage.tsx
+++ src/pages/TaskPage.tsx
@@ -1,52 +1,14 @@
-import React, { useEffect, useRef } from "react";
+// pages/TaskPage.tsx
+import React from "react";
 import BottomMenu from "../components/BottomMenu";
-import { THEME, TonConnectUI } from "@tonconnect/ui";
-import { createSwapWidget } from "@swap-coffee/ui-sdk";
 
 const TaskPage: React.FC = () => {
-  const manifestUrl = "https://swap.coffee/tonconnect-manifest.json";
-  const widgetInitialized = useRef(false);
-
-  const tonConnectSettings = {
-    manifestUrl: manifestUrl,
-    uiPreferences: {
-      theme: THEME.DARK,
-    },
-  };
-
-  useEffect(() => {
-    if (!widgetInitialized.current) {
-      const tonConnectUiInstance = new TonConnectUI(tonConnectSettings);
-
-      createSwapWidget("#swap-widget-component", {
-        theme: "light",
-        locale: "ru",
-        tonConnectManifest: {
-          url: manifestUrl,
-        },
-        injectionMode: "tonConnect",
-        tonConnectUi: tonConnectUiInstance,
-      });
-
-      widgetInitialized.current = true;
-    }
-  }, []);
-
   return (
     <div className="task-page">
       <h1>Task</h1>
       <p>Soon</p>
 
-      <div id="swap-widget-component"></div>
       <BottomMenu />
-
-      <style scoped>{`
-        #swap-widget-component {
-          margin: 0 auto;
-          width: 100%;
-          height: 500px;
-        }
-      `}</style>
     </div>
   );
 };
