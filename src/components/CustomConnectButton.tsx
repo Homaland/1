c6ef628a5fdfea20091e7cf7a3186356f54f9b6d
Homaland <168760948+Homaland@users.noreply.github.com>
2025-01-06 16:26:44 +0500

    Update CustomConnectButton.tsx

--- src/components/CustomConnectButton.tsx
+++ src/components/CustomConnectButton.tsx
@@ -1,17 +1,9 @@
 import { useTonConnectModal } from '@tonconnect/ui-react';
+import './CustomConnectButton.css'; // Вынесите стили в отдельный файл
 
 const CustomConnectButton = () => {
   const { open } = useTonConnectModal();
-<style>
-          {`
-             .custom-connect-button {
-              width: 100px;
-              height: 36px;
-              background-color:rgb(185, 82, 82);
-              border-radius: 18px;
-            }
-          `}
-        </style>
+
   return (
     <button onClick={open} className="custom-connect-button">
       Подключить кошелек
