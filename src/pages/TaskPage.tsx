ddc3a44a6cc95dbfd046a3336db56301419a9340
Homaland <168760948+Homaland@users.noreply.github.com>
2025-01-06 21:44:04 +0500

    Update TaskPage.tsx

--- src/pages/TaskPage.tsx
+++ src/pages/TaskPage.tsx
@@ -1,31 +1,19 @@
-
 import { useEffect, useRef } from 'react';
-import { THEME, TonConnectUI } from "@tonconnect/ui";
 import { createSwapWidget } from '@swap-coffee/ui-sdk';
 
 function App() {
     const manifestUrl = "https://swap.coffee/tonconnect-manifest.json";
     const widgetInitialized = useRef(false);
 
-    const tonConnectSettings = {
-        manifestUrl: manifestUrl,
-        uiPreferences: {
-            theme: THEME.DARK,
-        },
-    };
-
     useEffect(() => {
         if (!widgetInitialized.current) {
-            const tonConnectUiInstance = new TonConnectUI(tonConnectSettings);
-
             createSwapWidget('#swap-widget-component', {
                 theme: 'light',
                 locale: 'ru',
                 tonConnectManifest: {
                     url: manifestUrl,
                 },
-                injectionMode: 'tonConnect',
-                tonConnectUi: tonConnectUiInstance,
+                injectionMode: 'tonConnect', // Использует глобальный TonConnect
             });
 
             widgetInitialized.current = true;
