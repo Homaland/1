5e43fd0347c429f94d12b15bfee94a85cf908eb2
Homaland <168760948+Homaland@users.noreply.github.com>
2025-01-06 20:59:44 +0500

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,19 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
-
+import { SendJettonModal } from './SendJettonModal'; // Импортируем модальное окно
 import './Header.css';
 
-// Определяем типы пропсов
 interface HeaderProps {
   profilePhotoUrl: string | null;
   firstName: string | null;
+  jetton: JettonBalance;
+  senderAddress: Address;
+  jettons: JettonBalance[];
 }
 
-const Header = ({ profilePhotoUrl, firstName }: HeaderProps) => {
+const Header = ({ profilePhotoUrl, firstName, jetton, senderAddress, jettons }: HeaderProps) => {
   const [loading, setLoading] = useState(true);
+  const [isModalVisible, setIsModalVisible] = useState(false); // Состояние для модального окна
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -20,6 +23,10 @@ const Header = ({ profilePhotoUrl, firstName }: HeaderProps) => {
 
   const handleSettingsClick = () => {
     navigate('/settings');
+  };
+
+  const handleGiftClick = () => {
+    setIsModalVisible(true); // Открыть модальное окно
   };
 
   if (loading) {
@@ -47,7 +54,16 @@ const Header = ({ profilePhotoUrl, firstName }: HeaderProps) => {
           <i className="material-icons">arrow_forward_ios</i>
         </span>
       </div>
-    
+      <i className="material-icons gift-icon" onClick={handleGiftClick}>card_giftcard</i>
+
+      {/* Модальное окно для отправки жетонов */}
+      <SendJettonModal
+        jetton={jetton}
+        senderAddress={senderAddress}
+        onClose={() => setIsModalVisible(false)}
+        jettons={jettons}
+        isVisible={isModalVisible}
+      />
     </header>
   );
 };
